16c2066b553f3da16f573a0584d6e7e46b444ac1
Vũ Tuấn Anh <vtanh1905@gmail.com>
2020-05-19 14:04:17 +0700

    Browse Screen

---
+++ src/components/ListView/ListAuthor.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { StyleSheet, View, ScrollView, Text } from 'react-native';
+import { Avatar } from 'react-native-elements';
+
+function ListAuthor(props) {
+  const { title, data } = props;
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={{ fontWeight: "bold" }}>{title}</Text>
+      </View>
+      <View style={styles.listTag}>
+        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} >
+          {data.map((item) =>
+            <View style={{ paddingHorizontal: 5 }}>
+              <Avatar
+                rounded
+                size={100}
+                source={{
+                  uri: item.imageURL,
+                }}
+              />
+              <Text style={{ textAlign: "center", fontWeight: "bold" }}>{item.name}</Text>
+            </View>
+          )}
+        </ScrollView>
+      </View>
+    </View >
+  )
+}
+
+export default ListAuthor
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 10
+  },
+  listTag: {
+    flexDirection: "row",
+    padding: 5
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10
+  },
+  card: {
+    padding: 10
+  }
+})
--- src/screens/Main/Browse.js
+++ src/screens/Main/Browse.js
@@ -3,12 +3,43 @@ import { View } from 'react-native';
 
 import ListTag from '../../components/ListView/ListTag'
 
+import ListAuthor from '../../components/ListView/ListAuthor'
+
 const dataTag = ["Javascript", "Java", "C#", "C++", "Pythod", "React native"]
+
+const dataAuthor = [
+  {
+    name: "John Cneter",
+    imageURL: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg"
+  },
+  {
+    name: "John Cneter",
+    imageURL: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg"
+  },
+  {
+    name: "John Cneter",
+    imageURL: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg"
+  },
+  {
+    name: "John Cneter",
+    imageURL: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg"
+  },
+  {
+    name: "John Cneter",
+    imageURL: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg"
+  },
+  {
+    name: "John Cneter",
+    imageURL: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg"
+  }
+]
 
 function Browse() {
   return (
     <View>
       <ListTag title="Popular Skill" data={dataTag} />
+
+      <ListAuthor title="Top Author" data={dataAuthor} />
     </View >
   )
 }
