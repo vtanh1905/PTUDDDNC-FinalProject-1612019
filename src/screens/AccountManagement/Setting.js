8a7293dcd1d7e936d91310421818b22a02bf93b5
Vũ Tuấn Anh <vtanh1905@gmail.com>
2020-05-20 12:39:49 +0700

    Setting Screen

--- src/screens/AccountManagement/Setting.js
+++ src/screens/AccountManagement/Setting.js
@@ -1,12 +1,46 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { Subheading, Title, Avatar, Divider, Headline } from 'react-native-paper';
+import { Switch } from 'react-native-paper';
 
 function Setting() {
     return (
         <View>
-            <Text>Setting</Text>
+            <View style={styles.fieldView}>
+                <Subheading>Require Wifi for streamminh</Subheading >
+                <Switch
+                    value={true}
+                />
+            </View>
+
+            <View style={styles.fieldView}>
+                <Subheading>Require Wifi for dowwnloading</Subheading >
+                <Switch
+                    value={true}
+                />
+            </View>
+
+            <View style={styles.fieldView}>
+                <Subheading>Show quiz at the end of video</Subheading >
+                <Switch
+                    value={true}
+                />
+            </View>
+
         </View>
     )
 }
 
 export default Setting
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    fieldView: {
+        paddingHorizontal: 15,
+        paddingVertical: 5,
+        flexDirection: "row",
+        justifyContent: "space-between"
+    }
+})
